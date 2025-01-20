a2a246abba7a55b7a48e2cdd51e950b144d7e63c
Nikolai280293 <nikolaiosipov93@gmail.com>
2025-01-20 15:45:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ToDoList />
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="items">
+        <div class="item-top">
+            <input v-model="newTask" placeholder="Введите задачу" type="text">
+            <button v-on:click="addTask">Добавить</button>
+        </div>
+        <ol>
+            <li v-for="(task, index) in listItem" :key="index">
+                {{ task }}
+                <button v-on:click="removeTask(index)">Удалить</button>
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            listItem: [],
+            newTask: '',
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.trim()) {
+                this.listItem.push(this.newTask.trim());
+                this.newTask = '';
+            }
+        },
+        removeTask(index) {
+            this.listItem.splice(index, 1);
+        },
+    },
+}
+</script>
+
+<style>
+.items {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 12px;
+    width: 100%;
+}
+
+.item-top {
+    display: flex;
+    gap: 12px;
+}
+</style>
